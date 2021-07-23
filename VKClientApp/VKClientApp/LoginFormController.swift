5b9210ef8114417c52ae7f02ed37c07ff33cfc8a
Igor <i.gutchinov@gmail.com>
2021-07-23 12:42:25 +0300

    добавление функционала подъема экрана при отображении клавиатуры, а также наведение порядка после первого ДЗ на экране входа (исправлена привязка, альбомная ориентация на телефонах и планшетах)

--- VKClientApp/VKClientApp/LoginFormController.swift
+++ VKClientApp/VKClientApp/LoginFormController.swift
@@ -8,22 +8,60 @@
 import UIKit
 
 class LoginFormController: UIViewController {
+    
+    
+    // MARK: - Outlets
+    
+    // СкролВью
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    // Логин
+    @IBOutlet weak var loginInput: UITextField!
+    
+    // Пароль
+    @IBOutlet weak var passwordInput: UITextField!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // EnterButton
+    @IBOutlet weak var enterButton: UIButton!
+    
+    
+    // MARK: - Life cycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWasShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Functions
+    
+    @objc func keyBoardWasShow(_ notification: Notification) {
+        let info = notification.userInfo! as NSDictionary
+        let keyboardSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as? NSValue)?.cgRectValue.size
+        let contentInsent = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize?.height ?? 0, right: 0)
+        
+        scrollView.contentInset = contentInsent
+        scrollView.scrollIndicatorInsets = contentInsent
+        scrollView.scrollRectToVisible(enterButton.frame, animated: true)
     }
-    */
+    
+    @objc func keyBoardWillHide(_ notification: Notification) {
+        scrollView.contentInset = .zero
+    }
+    
+    
+    @IBAction func userSegmented(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func loginVersion(_ sender: Any) {
+    }
+    
+    @IBAction func buttonEnterPressed(_ sender: Any) {
+    }
+    
 
 }
